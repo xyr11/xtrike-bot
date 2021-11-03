d1022525c2533896d25066ba00673f8a14e13a0d
Xyrus Kurt Roldan <xyruskurt11@gmail.com>
2021-11-03 17:29:03 +0800

    added a "Did you mean" if didnt find a match

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require('discord.js')
+const Fuse = require('fuse.js')
 const { prefix, colors, hasPerms } = require('../config')
 
 exports.info = {
@@ -11,36 +12,38 @@ exports.info = {
 }
 
 exports.run = async (client, message, args) => {
+  const bot = await client.user.fetch() // for the bot avatar
+
   // if there are no args, get the help command of the help command itself
   if (args.length === 0) args[0] = 'help'
 
   // if there are args, get the first argument and search it up in commands list
   const cmd = client.commands.get(args[0])
 
-  // if that command doesn't exist, silently exit and do nothing
-  // if they dont have proper permLevels, do nothing too
-  // todo: if that command doesn't exist, say that that command doesn't exist / add those "did you mean x?" stuff in the future
+  // embed variable
+  const embed = new MessageEmbed().setFooter(`Xtrike Bot v${process.env.npm_package_version}`, bot.avatarURL())
+
   if (!cmd || !hasPerms(cmd, message)) {
-    message.reply({
-      embeds: [{
-        color: colors.main,
-        description: 'Sorry, we didn\'t find any command with\n' +
-          `the name \`${args[0]}\`. Please try again.`,
-        footer: { text: `Xtrike Bot v${process.env.npm_package_version}` }
-      }]
-    })
-    return
+    // if that command doesn't exist or if they dont have proper permission levels
+    const fuse = new Fuse(client.commands.map(a => a.info), { keys: ['name'] }) // search options
+    const results = fuse.search(args[0]) // search
+      .map(a => a.item.name) // get the command name only
+      .filter(a => hasPerms(client.commands.get(a), message)) // check if user has perms to view that command
+    // set the embed
+    embed.setColor(colors.main)
+      .setDescription('Sorry, we didn\'t find any commands with\n' +
+        `the name \`${args[0]}\`. ` + (
+        results.length
+          ? 'Did you mean:\n\n' + results.join('\n')
+          : 'Please try again.'))
+  } else {
+    const { name, description, usage, thumbnail } = cmd.info
+    // set the embed
+    embed.setColor(colors.main)
+      .setTitle(`${prefix}${name} command`)
+      .setThumbnail(thumbnail ?? '')
+      .setDescription(description)
+    if (usage) embed.addFields({ name: 'Usage', value: usage.replace(/^`/gm, '`' + prefix) })
   }
-
-  const { name, description, usage, thumbnail } = cmd.info
-
-  const bot = await client.user.fetch() // for the bot avatar
-  const embed = new MessageEmbed()
-    .setColor(colors.main)
-    .setTitle(`${prefix}${name} command`)
-    .setThumbnail(thumbnail ?? '')
-    .setDescription(description)
-    .setFooter(`Xtrike Bot v${process.env.npm_package_version}`, bot.avatarURL())
-  if (usage) embed.addFields({ name: 'Usage', value: usage.replace(/^`/gm, '`' + prefix) })
-  message.channel.send({ embeds: [embed] })
+  message.reply({ embeds: [embed] })
 }
