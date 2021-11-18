5a5e09d6d31745b41aa972fd813fa76c158a9ba9
Xyrus Kurt Roldan <xyruskurt11@gmail.com>
2021-11-19 01:23:59 +0800

    support for many links, fixed 'output.formats' bug

--- autoresponses/ytdlVideos.js
+++ autoresponses/ytdlVideos.js
@@ -20,78 +20,83 @@ module.exports = async message => {
     ')' +
     '(?=\\s|\\?|$)', 'g') // check if the next character is a space, a question mark or the end of the string
 
-  // get the link from the message
-  let link = message.content.match(ytdlRegex)
+  // get links from message
+  const links = message.content.match(ytdlRegex)
+
   // if there aren't any twitter links, return
-  if (!link || link.length < 1) return
-  // get first link only
-  link = link[0]
+  if (!links || !links.length) return
 
-  let output
-  // infinite loop
-  let noData = true
-  while (noData) {
-    try {
-      // fetch youtube-dl info
-      output = await youtubeDl(link, {
-        dumpSingleJson: true,
-        noWarnings: true
-      })
-      noData = false // stop the infinite loop
-    } catch (err) {
-      // retry if error is 'Unable to extract data' or error 500
-      if (err.stderr !== 'ERROR: Unable to extract data; please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; type  youtube-dl -U  to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.' && err.stderr !== "ERROR: Unable to download webpage: <urlopen error EOF occurred in violation of protocol (_ssl.c:600)> (caused by URLError(SSLEOFError(8, 'EOF occurred in violation of protocol (_ssl.c:600)'),))") {
-        console.log(err)
-        noData = false
+  for (const link of links) {
+    let output
+
+    // infinite loop
+    let noData = true
+    while (noData) {
+      try {
+        // fetch youtube-dl info
+        output = await youtubeDl(link, {
+          dumpSingleJson: true,
+          noWarnings: true
+        })
+        noData = false // stop the infinite loop
+      } catch (err) {
+        // retry if error is 'Unable to extract data' or error 500
+        if (err.stderr !== 'ERROR: Unable to extract data; please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; type  youtube-dl -U  to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.' && err.stderr !== "ERROR: Unable to download webpage: <urlopen error EOF occurred in violation of protocol (_ssl.c:600)> (caused by URLError(SSLEOFError(8, 'EOF occurred in violation of protocol (_ssl.c:600)'),))") {
+          await require('../modules/errorCatch')(err, message.client)
+          noData = false
+        }
       }
     }
-  }
 
-  // get the formats object
-  const videoLink = output.formats
-    .filter(a => a.protocol === 'https') // filter the .mp4 links
-    .map(a => a.url) // get the url of what's left
-
-  // check if there are extracted links
-  if (!videoLink || videoLink.length < 1) return
-
-  // create temp stream
-  const stream = temp.createWriteStream()
-
-  let data
-  // infinite loop
-  let notDownloaded = true
-  while (notDownloaded) {
-    try {
-      // download the video from the link given. (the zero index is the lowest quality version.)
-      data = await download(videoLink[1] ?? videoLink[0], { headers: output.http_headers })
-      notDownloaded = false // stop the infinite loop
-    } catch (err) {
-      // retry if error is error 500
-      if (err.code !== 500) {
-        notDownloaded = false
-        console.log(err)
-      }
+    // get the formats object
+    let videoLink = []
+    if (output) {
+      videoLink = output.formats
+        .filter(a => a.protocol === 'https') // filter the .mp4 links
+        .map(a => a.url) // get the url of what's left
     }
-  }
-  // write the data to the temp file
-  stream.write(data)
-  stream.end()
 
-  // create MessageAttachment
-  const file = new MessageAttachment(stream.path, `${new URL(link).pathname.replace(/\W+/g, '-').slice(1)}.mp4`)
-  // infinite loop
-  let notSent = true
-  while (notSent) {
-    try {
-      // reply it to the message (without pinging)
-      message.reply({ files: [file], allowedMentions: { repliedUser: false } })
-      notSent = false // stop the infinite loop
-    } catch (err) {
-      // retry if error is error 500
-      if (err.code !== 500) {
-        notSent = false
-        console.log(err)
+    // check if there are extracted links
+    if (videoLink.length) {
+      // create temp stream
+      const stream = temp.createWriteStream()
+
+      let data
+      // infinite loop
+      let notDownloaded = true
+      while (notDownloaded) {
+        try {
+          // download the video from the link given. (the zero index is the lowest quality version.)
+          data = await download(videoLink[1] ?? videoLink[0], { headers: output.http_headers })
+          notDownloaded = false // stop the infinite loop
+        } catch (err) {
+          // retry if error is error 500
+          if (err.code !== 500) {
+            notDownloaded = false
+            console.log(err)
+          }
+        }
+      }
+      // write the data to the temp file
+      stream.write(data)
+      stream.end()
+
+      // create MessageAttachment
+      const file = new MessageAttachment(stream.path, `${new URL(link).pathname.replace(/\W+/g, '-').slice(1)}.mp4`)
+      // infinite loop
+      let notSent = true
+      while (notSent) {
+        try {
+          // reply it to the message (without pinging)
+          message.reply({ files: [file], allowedMentions: { repliedUser: false } })
+          notSent = false // stop the infinite loop
+        } catch (err) {
+          // retry if error is error 500
+          if (err.code !== 500) {
+            notSent = false
+            console.log(err)
+          }
+        }
       }
     }
   }
