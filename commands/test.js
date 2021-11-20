90ac0078442c175c0946cee727ed2e3b8a27fd8e
Xyrus Kurt Roldan <xyruskurt11@gmail.com>
2021-11-20 18:11:42 +0800

    debugging in slash commands now works, "debug" option now returns serialized object

--- commands/test.js
+++ commands/test.js
@@ -1,4 +1,4 @@
-const { Message, Interaction } = require('discord.js') // eslint-disable-line no-unused-vars
+const { Message, Interaction, MessageEmbed } = require('discord.js') // eslint-disable-line no-unused-vars
 
 exports.info = {
   name: 'test',
@@ -23,21 +23,35 @@ exports.info = {
  */
 exports.run = (message, interaction, args) => {
   const thing = message || interaction
-  if (interaction) console.log(interaction)
 
+  // reply
   thing.reply(`Test received! ${args.length ? '\nargs: ' + args.join(',') : ''}`)
+
+  /** @type {MessageEmbed} */
+  let debugEmbed
+  // debug
   if (args[0] === 'embed') {
     // should produce a ReferenceError error
     thing.reply(testing_the_error_embed_dont_mind) // eslint-disable-line no-undef
   } else if (args[0] === 'debug') {
-    thing.reply({ embeds: [{ description: '```' + JSON.stringify(thing, undefined, 2) + '```' }] })
+    // Message / Interaction
+    const { ...serialized } = thing
+    debugEmbed = { description: '```' + JSON.stringify(serialized, (key, val) => typeof val === 'bigint' ? val.toString() : val, 2) + '```' }
   } else if (args[0] === 'author') {
-    thing.reply({ embeds: [{ description: '```' + JSON.stringify(thing.author, undefined, 2) + '```' }] })
+    // author User
+    debugEmbed = { description: '```' + JSON.stringify(thing.author, undefined, 2) + '```' }
   } else if (args[0] === 'bot') {
-    thing.reply({ embeds: [{ description: '```' + JSON.stringify(thing.client.user, undefined, 2) + '```' }] })
+    // client bot User
+    debugEmbed = { description: '```' + JSON.stringify(thing.client.user, undefined, 2) + '```' }
   } else if (args[0] === 'commands') {
+    // output all commands
     console.log(thing.client.commands)
   } else if (args[0] === 'help') {
-    for (const cmd of thing.client.commands.map(a => a.info.name)) thing.client.commands.get('help').run(thing, false, [cmd])
+    // return the help info of each command
+    if (message) for (const cmd of thing.client.commands.map(a => a.info.name)) thing.client.commands.get('help').run(message, null, [cmd])
+  }
+  if (debugEmbed) {
+    if (interaction) interaction.followUp({ embeds: [debugEmbed] })
+    else message.reply({ embeds: [debugEmbed] })
   }
 }
