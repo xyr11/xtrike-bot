e180956a928e58e4b88c3e59ac9670b44e39e796
Xyrus Kurt Roldan <xyruskurt11@gmail.com>
2021-11-24 10:32:11 +0800

    load new commands and remove deleted commands

--- commands/reload.js
+++ commands/reload.js
@@ -7,10 +7,22 @@ exports.info = {
   name: 'reload',
   category: 'Developer',
   description: 'Reload a command',
-  usage: '`$$reload <command name>`',
-  aliases: ['refresh'],
+  usage: '`$$reload <command>`',
+  aliases: ['refresh', 'load'],
   permLevel: 'lmao',
   requiredArgs: true
+}
+
+/**
+ * check if file exists
+ * @param {fs.PathLike} filePath path of the file
+ * @returns {Boolean|undefined}
+ */
+const doesExist = async filePath => {
+  try {
+    await fs.promises.access(filePath)
+    return true
+  } catch (err) {}
 }
 
 /**
@@ -24,7 +36,7 @@ exports.run = async (message, interaction, args) => {
 
   try {
     // name of the module that will be reloaded
-    let reloadedName = ''
+    let reloadedName
     // Check if bot will reload autoresponses
     // from https://github.com/AnIdiotsGuide/guidebot/ (commands/reload.js), MIT License
     if (commandName === 'auto') {
@@ -40,26 +52,38 @@ exports.run = async (message, interaction, args) => {
         client.autoresponses.push(autoresponse)
         client.autoresponseNames.push(file.split('.')[0])
       }
-      reloadedName = 'Autoresponses'
+      reloadedName = 'All autoresponses have been reloaded'
     } else if (commandName === 'err') {
       // delete cache
       delete require.cache[require.resolve('../modules/errorCatch.js')]
       // re-add the module
       require('../modules/errorCatch.js')
-      reloadedName = 'The errorCatch module'
+      reloadedName = 'The errorCatch module has been reloaded'
     } else {
-      // Check if the command exists and is valid
-      if (!client.commands.has(commandName)) return message.reply('That command does not exist')
-      delete require.cache[require.resolve(`./${commandName}.js`)]
-      // We also need to delete and reload the command from the client.commands Enmap
-      client.commands.delete(commandName)
-      // re-add the module
-      const props = require(`./${commandName}.js`)
-      client.commands.set(commandName, props)
-      reloadedName = `The command \`${prefix}${commandName}\``
+      const fileExists = await doesExist(`./commands/${commandName}.js`)
+      // Check if the command can be found in client.commands
+      if (!client.commands.has(commandName)) {
+        // check if file of the command exists
+        if (!fileExists) return message.reply('That command does not exist')
+        // add the module
+        const props = require(`./${commandName}.js`)
+        client.commands.set(commandName, props)
+        reloadedName = `The command \`${prefix}${commandName}\` has been successfully loaded`
+      } else {
+        // delete the command on client.commands
+        client.commands.delete(commandName)
+        // check if file of the command is deleted
+        if (!fileExists) return message.reply('Command has been deleted')
+        // delete cache
+        delete require.cache[require.resolve(`./${commandName}.js`)]
+        // re-add the module
+        const props = require(`./${commandName}.js`)
+        client.commands.set(commandName, props)
+        reloadedName = `The command \`${prefix}${commandName}\` has been reloaded`
+      }
     }
-    message.reply(`${reloadedName} has been reloaded`)
-    console.log(chalk.green(`${reloadedName} has been reloaded`, chalk.bgGreenBright.black(`(${time()})`)))
+    message.reply(reloadedName)
+    console.log(chalk.green(reloadedName), chalk.bgGreenBright.black(time()))
   } catch (error) {
     await require('../modules/errorCatch')(error, message.client, message)
     await message.reply('I have encountered an error while trying to reload. \n⚠️ Because the reload command is very powerful, this error can cause massive disruption. Therefore, the bot will REBOOT shortly. ⚠️')
