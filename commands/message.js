30102613d768b5ec96ab5a993829d6de6ccc8018
Xyrus Kurt Roldan <xyruskurt11@gmail.com>
2021-11-25 18:23:12 +0800

    revamped, added support for creating embeds

--- commands/message.js
+++ commands/message.js
@@ -1,11 +1,14 @@
-const { Message } = require('discord.js') // eslint-disable-line no-unused-vars
+const { Message, MessageEmbed, MessagePayload } = require('discord.js') // eslint-disable-line no-unused-vars
 
 exports.info = {
   name: 'message',
   category: 'Developer',
   description: 'Message as the bot',
-  usage: '`$$message <text> [messageId]`',
-  option: '`messageId` is the message you want to reply to (optional)',
+  usage: '`$$message [messageId] <content> <embed>`',
+  option: '`[messageId]` is the message you want to reply to (optional)\n' +
+    '`<content>` is the content of the message\n' +
+    '`<embed>` is an embed constructor. Example: `$t Title $d Description $a Author $u Url (link) $c Color (hex) $i Image (link) $h Thumbnail (link) $f Footer $s Timestamp (Unix time)`. You can remove extra options, but a title (`$t`) or a description (`$d`) must be present. \n\n' +
+    'Sample command: `;message Content $d Description $a Author $f Footer $s`',
   aliases: ['msg'],
   permLevel: 'lmao',
   requiredArgs: true
@@ -16,26 +19,73 @@ exports.info = {
  * @param {Array} args
  */
 exports.run = async (message, interaction, args) => {
-  if (!args[0]) return
+  const channel = message.channel // current channel
 
-  // the text to reply
-  const text = args[0].replaceAll("''", ' ')
-  // current channel
-  const channel = message.channel
+  // text parser
+  /** @type {String} */
+  const text = args.join(' ')
+  // sample input: `;message 802494274238808105 Content $t Title $d Description $a author $c #000 $s $i example.com`
 
-  if (args[1]) {
-    // reply to channel id given on args[1]
-    const messages = await channel.messages.fetch({ limit: 100 })
-    const fetchedMsg = messages.get(args[1])
-    if (fetchedMsg) await fetchedMsg.reply(text)
-  } else {
-    // normal message
-    await channel.send(text)
+  // get message id of the message to reply to, if there's anything
+  // valid channel snowflakes has 17-20 characters comprised solely of numbers
+  // see https://discord.com/developers/docs/reference#snowflakes and https://snowsta.mp/
+  // tl;dr: min possible value of a snowflake is 47835198259200000 (March 13 2015, founding of discord),
+  //        max is 18446744073709551615 (all 65 bytes are used)
+  const replyMsg = text.search(/[0-9]{17,21}/) === 0 ? text.match(/[0-9]{8,25}\s*/g)[0] : ''
+
+  // get message content
+  let content = text.substring(replyMsg.length).match(/^((?!\$[A-z]).)+(?=\$[A-z])/)
+  content = content ? content[0] : ''
+
+  // setup embed
+  const textEmbed = text.substring(replyMsg.length + content.length)
+  const embRgx = {
+    // always get the first option
+    setTitle: /(?<=\$t *)((?!\$[A-z]).)+/s, //       title:       $t
+    setDescription: /(?<=\$d *)((?!\$[A-z]).)+/s, // description: $d
+    setAuthor: /(?<=\$a *)((?!\$[A-z]).)+/s, //      author:      $a
+    setURL: /(?<=\$u *)((?!\$[A-z]).)+/s, //         Url:         $u
+    setColor: /(?<=\$c *)((?!\$[A-z]).)+/s, //       color:       $c
+    setFooter: /(?<=\$f *)((?!\$[A-z]).)+/s, //      footer:      $f
+    setImage: /(?<=\$i *)((?!\$[A-z]).)+/s, //       image:       $i
+    setThumbnail: /(?<=\$h *)((?!\$[A-z]).)+/s //    tHumbnails:  $h
+    // TODO: fields
   }
-  message.delete().catch(err => {
-    if (err.message === 'Missing Permissions') {
-      return message.author.send(`I cannot delete your message because I have missing permissions in <#${message.channelId}>. You can delete your own message instead.`)
+  let embed
+  if (textEmbed.match(embRgx.setTitle) || textEmbed.match(embRgx.setDescription)) {
+    embed = new MessageEmbed()
+    // check each property of embRgx
+    for (const type of Object.keys(embRgx)) {
+      // if there is a match then set that value to the embed
+      if (textEmbed.match(embRgx[type])) embed[type](textEmbed.match(embRgx[type])[0].replace(/^\s*|\s*$/gs, ''))
     }
+    // manually set the timestamp option
+    const textEmbHasTime = textEmbed.match(/(?<=\$s *)((?!\$[A-z]).)*/s) // check if there is $s
+    if (textEmbHasTime) embed.setTimestamp(textEmbHasTime[0].replace(/^\s*|\s*$/gs, '') || Date.now()) // if there is a value after $s then use that value, if there are no values then use the current time
+  }
+
+  // prepare message
+  /** @type {MessagePayload} */
+  const finalMessage = {}
+  if (content) finalMessage.content = content
+  if (embed) finalMessage.embeds = [embed]
+
+  // fetch the message if there is a given message id
+  let fetchedMsg
+  if (replyMsg) {
+    const messages = await channel.messages.fetch({ limit: 100 })
+    fetchedMsg = messages.get(replyMsg.replace(/^\s*|\s*$/gs, ''))
+  }
+
+  // if the message is found
+  if (fetchedMsg) await fetchedMsg.reply(finalMessage)
+  // if the message is not found or there are no given message id
+  else await channel.send(finalMessage)
+
+  // delete user message
+  message.delete().catch(err => {
+    // if the bot doesn't have permissions then dm the user
+    if (err.message === 'Missing Permissions') return message.author.send(`I cannot delete your message because I have missing permissions in <#${channel.id}>`)
     require('../modules/errorCatch')(err, message.client, message)
   })
 }
