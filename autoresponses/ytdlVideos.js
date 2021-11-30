b314d06c4e6b9b6fafc0a029d4cb6a9861f16644
Xyrus Kurt Roldan <xyruskurt11@gmail.com>
2021-11-30 12:50:28 +0800

    ignore unsupported link error, fixed ytdlVideos.js
    fixes for ytdlVideos.js:
    - added `continue`
    - fixed output.formats bug (again)
    - log errors using errorCatch.js

--- autoresponses/ytdlVideos.js
+++ autoresponses/ytdlVideos.js
@@ -2,9 +2,15 @@ const { Message, MessageAttachment } = require('discord.js') // eslint-disable-l
 const download = require('download')
 const temp = require('temp')
 const youtubeDl = require('youtube-dl-exec')
+const errorCatch = require('../modules/errorCatch')
 
 // Automatically track and cleanup files at exit
 temp.track()
+
+const retryIfErr = [
+  'ERROR: Unable to extract data; please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; type  youtube-dl -U  to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.',
+  "ERROR: Unable to download webpage: <urlopen error EOF occurred in violation of protocol (_ssl.c:600)> (caused by URLError(SSLEOFError(8, 'EOF occurred in violation of protocol (_ssl.c:600)'),))"
+]
 
 /**
  * Check for video links and send the raw video file using youtube-dl
@@ -27,76 +33,82 @@ module.exports = async message => {
   if (!links || !links.length) return
 
   for (const link of links) {
+    // get youtube-dl info
+    // this part is an infinite loop, so if it encounters an error then
+    // it will repeat. if there are no errors then the loop will break.
     let output
-
-    // infinite loop
     let noData = true
     while (noData) {
       try {
-        // fetch youtube-dl info
-        output = await youtubeDl(link, {
-          dumpSingleJson: true,
-          noWarnings: true
-        })
+        // fetch
+        output = await youtubeDl(link, { dumpSingleJson: true, noWarnings: true })
         noData = false // stop the infinite loop
       } catch (err) {
-        // retry if error is 'Unable to extract data' or error 500
-        if (err.stderr !== 'ERROR: Unable to extract data; please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; type  youtube-dl -U  to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.' && err.stderr !== "ERROR: Unable to download webpage: <urlopen error EOF occurred in violation of protocol (_ssl.c:600)> (caused by URLError(SSLEOFError(8, 'EOF occurred in violation of protocol (_ssl.c:600)'),))") {
-          await require('../modules/errorCatch')(err, message.client)
+        // check if error is 'Unable to extract data' or error 500, in
+        // which it will fetch again
+        if (retryIfErr.indexOf(err.stderr) === -1) {
+          // if not then stop the infinite loop and log the error
+          errorCatch(err, message.client)
           noData = false
         }
       }
     }
+    // continue to the next link if youtube-dl doesn't return an output
+    if (!output) continue
 
-    // get the formats object
-    let videoLink = []
-    if (!output.formats) console.log(output) // debug output if there is no formats
+    // get the download links
+    let videoLinks = []
     if (output && output.formats) {
-      videoLink = output.formats
+      videoLinks = output.formats
         .filter(a => a.protocol === 'https') // filter the .mp4 links
         .map(a => a.url) // get the url of what's left
     }
+    // continue to the next link if there are no available links
+    if (!videoLinks.length) continue
 
-    // check if there are extracted links
-    if (videoLink.length) {
-      // create temp stream
-      const stream = temp.createWriteStream()
+    // create temp stream
+    const stream = temp.createWriteStream()
 
-      let data
-      // infinite loop
-      let notDownloaded = true
-      while (notDownloaded) {
-        try {
-          // download the video from the link given (the zero index is the lowest quality version)
-          data = await download(videoLink[0], { headers: output.http_headers })
-          notDownloaded = false // stop the infinite loop
-        } catch (err) {
-          // retry if error is error 500
-          if (err.code !== 500) {
-            notDownloaded = false
-            console.log(err)
-          }
+    // download the video from the link given (zero index is the lowest quality)
+    // this part is an infinite loop, so if it encounters an error then
+    // it will repeat. if there are no errors then the loop will break.
+    let data
+    let notDownloaded = true
+    while (notDownloaded) {
+      try {
+        // download
+        data = await download(videoLinks[0], { headers: output.http_headers })
+        notDownloaded = false // stop the infinite loop
+      } catch (err) {
+        // check if error is error 500 in which it will repeat again
+        if (err.code !== 500) {
+          // if not then log the error
+          errorCatch(err, message.client)
+          notDownloaded = false
         }
       }
-      // write the data to the temp file
-      stream.write(data)
-      stream.end()
+    }
+    // continue to the next link if video isn't downloaded
+    if (!data) continue
 
-      // create MessageAttachment
-      const file = new MessageAttachment(stream.path, `${new URL(link).pathname.replace(/\W+/g, '-').slice(1)}.mp4`)
-      // infinite loop
-      let notSent = true
-      while (notSent) {
-        try {
-          // reply it to the message (without pinging)
-          message.reply({ files: [file], allowedMentions: { repliedUser: false } })
-          notSent = false // stop the infinite loop
-        } catch (err) {
-          // retry if error is error 500
-          if (err.code !== 500) {
-            notSent = false
-            console.log(err)
-          }
+    // write the data to the temp file
+    stream.write(data)
+    stream.end()
+    // create MessageAttachment
+    const file = new MessageAttachment(stream.path, `${new URL(link).pathname.replace(/\W+/g, '-').slice(1)}.mp4`)
+    // send the video
+    // this part is an infinite loop too
+    let notSent = true
+    while (notSent) {
+      try {
+        message.reply({ files: [file], allowedMentions: { repliedUser: false } }) // reply to the message (without pinging)
+        notSent = false // stop the infinite loop
+      } catch (err) {
+        // check if error is error 500 in which it will repeat again
+        if (err.code !== 500) {
+          // if not then log the error
+          errorCatch(err, message.client)
+          notSent = false
         }
       }
     }
