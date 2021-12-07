2ac93036cac6bfc0fdab04529af8b5b61233e429
Xyrus Kurt Roldan <xyruskurt11@gmail.com>
2021-12-07 20:12:52 +0800

    `test` command updates ++ - added more interaction options for testing - added 'options' option for debugging interaction options - cleanup 🧹

--- commands/test.js
+++ commands/test.js
@@ -8,11 +8,14 @@ exports.info = {
   aliases: ['tests'],
   permLevel: 'User',
   options: [
-    {
-      type: 3,
-      name: 'test',
-      description: 'Test'
-    }
+    { type: 3, name: 'string', description: 'Test' },
+    { type: 4, name: 'integer', description: 'test' },
+    { type: 5, name: '--flag', description: 'test' },
+    { type: 6, name: 'user', description: 'test' },
+    { type: 7, name: 'channel', description: 'test' },
+    { type: 8, name: 'role', description: 'test' },
+    { type: 9, name: 'mentionable', description: 'test' },
+    { type: 10, name: 'number', description: 'test' }
   ]
 }
 
@@ -21,11 +24,12 @@ exports.info = {
  * @param {Interaction} interaction
  * @param {Array} args
  */
-exports.run = (message, interaction, args) => {
+exports.run = async (message, interaction, args) => {
   const thing = message || interaction
+  const client = thing.client
 
   // reply
-  thing.reply(`Test received! ${args.length ? '\nargs: ' + args.join(',') : ''}`)
+  await thing.reply(`Test received! ${args.length ? '\nargs: ' + args.join(',') : ''}`)
 
   /** @type {MessageEmbed} */
   let debugEmbed
@@ -34,24 +38,30 @@ exports.run = (message, interaction, args) => {
     // should produce a ReferenceError error
     thing.reply(testing_the_error_embed_dont_mind) // eslint-disable-line no-undef
   } else if (args[0] === 'debug') {
-    // Message / Interaction
+    // message/interaction object
     const { ...serialized } = thing
     debugEmbed = { description: '```' + JSON.stringify(serialized, (key, val) => typeof val === 'bigint' ? val.toString() : val, 2) + '```' }
   } else if (args[0] === 'author') {
-    // author User
+    // author user
     debugEmbed = { description: '```' + JSON.stringify(thing.author, undefined, 2) + '```' }
   } else if (args[0] === 'bot') {
-    // client bot User
-    debugEmbed = { description: '```' + JSON.stringify(thing.client.user, undefined, 2) + '```' }
+    // client bot user
+    debugEmbed = { description: '```' + JSON.stringify(client.user, undefined, 2) + '```' }
   } else if (args[0] === 'commands') {
     // output all commands
-    console.log(thing.client.commands)
+    console.log(client.commands)
   } else if (args[0] === 'help') {
     // return the help info of each command
-    if (message) for (const cmd of thing.client.commands.map(a => a.info.name)) thing.client.commands.get('help').run(message, null, [cmd])
+    if (message) {
+      for (const cmd of client.commands.map(a => a.info.name)) client.commands.get('help').run(message, null, [cmd])
+    }
+  } else if (args[0] === 'options') {
+    // show interaction options
+    if (interaction) debugEmbed = { description: '```' + JSON.stringify(interaction.options.data, undefined, 2) + '```' }
   }
-  if (debugEmbed) {
-    if (interaction) interaction.followUp({ embeds: [debugEmbed] })
-    else message.reply({ embeds: [debugEmbed] })
-  }
+
+  // send debug messages
+  if (!debugEmbed) return
+  if (interaction) interaction.followUp({ embeds: [debugEmbed] })
+  else message.send({ embeds: [debugEmbed] })
 }
