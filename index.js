5438e52d1ef4599cf0864bd03df256fcf925eee1
Xyrus Kurt Roldan <xyruskurt11@gmail.com>
2021-12-08 05:49:22 +0800

    dont console.log every module anymore

--- index.js
+++ index.js
@@ -19,12 +19,11 @@ client.on('error', error => outputErr(error, client))
 // read the commands folder
 client.commands = new Collection()
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+console.log(chalk.gray('Loading commands'))
 for (const file of commandFiles) {
-  const commandName = file.split('.')[0]
   // Add module of each of the command files
   const command = require(`./commands/${file}`)
   // Load em
-  console.log(chalk.gray(`Loading the ${commandName} command`))
   client.commands.set(command.info.name, command)
 }
 
@@ -32,25 +31,24 @@ for (const file of commandFiles) {
 client.autoresponses = []
 client.autoresponseNames = []
 const autoresponseFiles = fs.readdirSync('./autoresponses').filter(file => file.endsWith('.js'))
+console.log(chalk.gray('Loading autoresponses'))
 for (const file of autoresponseFiles) {
-  const autoresponseName = file.split('.')[0]
+  const name = file.split('.')[0]
   // Add module of each of the autoresponse files
   const autoresponse = require(`./autoresponses/${file}`)
-  // Load em
-  console.log(chalk.gray(`Loading the ${autoresponseName} autoresponse`))
   // Add to arrays
   client.autoresponses.push(autoresponse)
-  client.autoresponseNames.push(autoresponseName)
+  client.autoresponseNames.push(name)
 }
 
 // read the events folder
 const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
+console.log(chalk.gray('Loading events'))
 for (const file of events) {
   const eventName = file.split('.')[0]
   // Add module of each of the event files
   const event = require(`./events/${file}`)
   // Load em
-  console.log(chalk.gray(`Loading the ${eventName} event`))
   client.on(eventName, (...args) => event.execute(...args))
 }
 
