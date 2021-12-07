0f7110ac632ec499b4c9e81ff0208ab3355542a1
Xyrus Kurt Roldan <xyruskurt11@gmail.com>
2021-12-07 20:02:57 +0800

    `interactionCreate` updates and fixes ++ - use `interaction.options.data` to get options - better `options` perser and support for all 8 option types - retry if "Unknown Interaction" error - some cleanup 🧹

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -7,36 +7,46 @@ exports.execute = async interaction => {
   if (!interaction.isCommand()) return
   const { client } = interaction
 
+  // get command
   const cmd = client.commands.get(interaction.commandName)
 
-  // If that command doesn't exist, silently exit and do nothing
-  // If they dont have proper permLevels, do nothing too
+  // if that command doesn't exist, silently exit and do nothing
+  // if they dont have proper permLevels, do nothing too
   if (!cmd || !hasPerms(cmd, interaction)) return
 
-  // Check if beta
-  if (cmd.info.isBeta === true && userPerms(interaction) < 4) {
-    interaction.reply('This command isn\'t really done yet, check back later.')
-    return
+  // check if beta
+  if (cmd.info.isBeta && userPerms(interaction) < 4) {
+    return interaction.reply('This command isn\'t really done yet, check back later.')
   }
 
   // create args from interaction.options
   const args = []
-  // this grabs the options into "/image words: test --server: true --all: true"
-  // and makes it into ";image --server --all test"
-  for (const o of interaction.options._hoistedOptions) {
-    if (o.name.search('--') === 0 && o.value) args.push(o.args)
-    else {
-      if (o.type === 'STRING') o.value.split(/ +/g).forEach(e => args.push(e))
-      else if (o.type === 'CHANNEL') args.push(`<#${o.value}>`)
-      else args.push(o.value)
-    }
+  for (const o of interaction.options.data) {
+    // if the option is a boolean and the value is true, add the option name in the args
+    if (o.type === 'BOOLEAN' && o.value) args.push(o.name)
+    // if the option is a string, add the option value
+    else if (o.type === 'STRING') args.push(...o.value.split(/ +/g)) // split each words using spaces
+    // if the option is a number, convert to string
+    else if (o.type === 'INTEGER' || o.type === 'NUMBER') args.push(o.value + '') // split each words using spaces
+    // if the option is a channel, convert it to the `<#channelId>` format
+    else if (o.channel) args.push(`<#${o.value}>`)
+    // if the option is a user, convert it to the `<@id>` format
+    else if (o.user) args.push(`<@${o.value}>`)
+    // if the option is a role, convert it to the `<@id>` format
+    else if (o.role) args.push(`<@&${o.value}>`)
   }
+  // add the content property on `inteaction` for commands that need it
   interaction.content = `/${interaction.commandName} ${args.join(' ')}`
 
-  try {
-    cmd.run(null, interaction, args)
-    recordStats(cmd.info.name)
-  } catch (error) {
-    require('../modules/errorCatch')(error, client, null, interaction)
+  while (true) {
+    try {
+      // execute command
+      await cmd.run(null, interaction, args)
+      return recordStats(cmd.info.name) // record statistics
+    } catch (error) {
+      // retry if "Unknown Interaction" error
+      // if not then throw an error and log it
+      if (error.message !== 'Unknown Interaction') return require('../modules/errorCatch')(error, client, null, interaction)
+    }
   }
 }
