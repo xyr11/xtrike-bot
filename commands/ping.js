9a7a448e0153a16d1981c3b7b81ec0c157abc5c2
Xyrus Kurt Roldan <xyruskurt11@gmail.com>
2021-12-13 23:38:51 +0800

    updated embed text

--- commands/ping.js
+++ commands/ping.js
@@ -29,7 +29,7 @@ exports.run = async (msg, args) => {
 
   // send initial message
   await msg.setDefer()
-  await msg.reply({ embeds: [{ description: ':person_running: Pinging...' }] })
+  await msg.reply({ embeds: [{ description: 'Loading...' }] })
 
   // number of loops
   const loops = (Number(args[0]) && args[0] > 0 && args[0] < 31) ? args[0] : 3
