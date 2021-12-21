543df8114fda9d4a4cd05b045a3fb46ee16f6911
Xyrus Kurt Roldan <xyruskurt11@gmail.com>
2021-12-21 17:36:11 +0800

    use msg.text instead of args, 🧹

--- commands/hmm.js
+++ commands/hmm.js
@@ -5,19 +5,8 @@ exports.info = {
   usage: '`$$hmm [extra text]`',
   aliases: ['hmmm', 'hmmmm', 'hmmmmm'],
   permLevel: 'User',
-  options: [
-    {
-      type: 3,
-      name: 'text',
-      description: 'Extra text'
-    }
-  ]
+  options: [{ type: 3, name: 'text', description: 'Extra text' }]
 }
 
-/**
- * @param {import('../modules/sendMsg')} msg
- * @param {Array=} args
- */
-exports.run = (msg, args) => args.length
-  ? msg.send(`${msg.author} ${args.join(' ')} hmmmm... :thinking:`)
-  : msg.send(`${msg.author} hmmmm... :thinking:`)
+/** @param {import('../modules/sendMsg')} msg */
+exports.run = msg => msg.send(`${msg.author} ${msg.text ? msg.text + ' ' : ''}hmmmm... 🤔`)
