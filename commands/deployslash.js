e1b1de81ae7f7eb06291c1b19c534f9c2a262d8d
xyr11 <68799333+xyr11@users.noreply.github.com>
2022-01-30 22:15:00 +0800

    deploySlash is deprecated, dont rely to it anymore

--- commands/deployslash.js
+++ commands/deployslash.js
@@ -1,44 +1,37 @@
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
-const { discordToken, clientId, deploySlash, testingServer } = require('../config')
-const { PermLevels } = require('../modules/base')
+const { discordToken, clientId, testingServer } = require('../config')
+const { registerSlashCommandsBody } = require('../modules/base')
 
 exports.info = {
   name: 'deployslash',
   category: 'Developer',
   description: 'Deploy slash commands.',
   usage: '`$$deployslash`',
-  aliases: [],
-  permLevel: 'lmao'
+  option: '`--all` to deploy on all servers (may take up to an hour)',
+  permLevel: 'lmao',
+  options: []
 }
 
-/** @param {import('../class/sendMsg')} msg */
-exports.run = async msg => {
+/**
+ * @param {import('../class/sendMsg')} msg
+ * @param {Array} args
+ */
+exports.run = async (msg, args) => {
   // Check if there is a test server given
-  if (!deploySlash && !testingServer) throw new Error('No test server id was found in your config.js')
+  if (!testingServer) throw new Error('No test server id was found in your config.js')
 
   await msg.reply('Deploying slash commands...')
-
-  // Get each command
-  const body = Array.from(msg.client.commands, ([name, value]) => value.info)
-    .filter(a => PermLevels[a.permLevel].level < 4) // filter commands available to bot admins and below
-    .map(({ name, description, options }) => ({ name, description, options })) // get the name, description, and options properties
-  // Clean description field
-  body.forEach(a => {
-    a.description = a.description.replace(/{{((?!}}).)+}}/g, ' ') // remove all stuff enclosed by `{{` and `}}`
-      .replace(/(\s|\n)+/g, ' ') // remove newlines and double spaces
-      .replace(/^ *| *$/g, '') // remove extra spaces before and after the string
-  })
 
   // Deploy
   const rest = new REST({ version: '9' }).setToken(discordToken)
   rest.put(
-    deploySlash
+    args[0] === '--all'
       ? Routes.applicationCommands(clientId) // deploy in all guilds
       : Routes.applicationGuildCommands(clientId, testingServer), // deploy in bot server
-    { body }
+    { body: registerSlashCommandsBody(msg.client) }
   ).then(() => {
-    msg.send('Successfully registered application commands')
-    msg.info('Successfully registered application commands')
+    msg.send('Successfully deployed application commands!')
+    msg.info('Registered application commands', args[0] === '--all' ? 'in all guilds' : '')
   })
 }
