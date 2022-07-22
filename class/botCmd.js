89f335f6e0357f76512b0d2692091d8d58c05111
xyr11 <68799333+xyr11@users.noreply.github.com>
2022-07-22 10:23:17 +0800

    rename methods and update documentation of BotCmd

--- class/botCmd.js
+++ class/botCmd.js
@@ -1,86 +1,85 @@
-/**
- * [Source documentation](https://discord.com/developers/docs/interactions/application-commands)
- *
- * @typedef {Object} applicationCommandOptionChoice If you specify `choices` for an option, they are the only valid values for a user to pick.
- * @prop {String} name 1-100 character choice name
- * @prop {String|Number} value value of the choice, up to 100 characters if string
- *
- * @typedef {Object} applicationCommandOption The parameters for the slash command.
- * @prop {1|2|3|4|5|6|7|8|9|10} type the type of option
- * - 1: `SUB_COMMAND`
- * - 2: `SUB_COMMAND_GROUP`
- * - 3: `STRING`
- * - 4: `INTEGER`	(Any integer between -2^53 and 2^53)
- * - 5: `BOOLEAN`
- * - 6: `USER`
- * - 7: `CHANNEL`	(Includes all channel types + categories)
- * - 8: `ROLE`
- * - 9: `MENTIONABLE`	(Includes users and roles)
- * - 10: `NUMBER`	(Any double between -2^53 and 2^53)
- * @prop {String} name 1-32 character name
- * - Command names must match the following regex `^[\w-]{1,32}$` with the unicode flag set
- * - If there is a lowercase variant of any letters used, you must use those
- * @prop {String} description 1-100 character description
- * @prop {Boolean} [required] if the parameter is required or optional -- default `false`
- * @prop {applicationCommandOptionChoice[]} [choices] choices for `STRING`, `INTEGER`, and `NUMBER` types for the user to pick from, max 25
- * @prop {applicationCommandOption[]} [options] if the option is a subcommand or subcommand group type, these nested options will be the parameters
- * @prop {Number[]} [channel_types] if the option is a `CHANNEL` type, the channels shown will be restricted to these types
- * @prop {Number} [min_value] if the option is an `INTEGER` or `NUMBER` type, the minimum value permitted
- * @prop {Number} [max_value] if the option is an `INTEGER` or `NUMBER` type, the maximum value permitted
- * @prop {Boolean} [autocomplete] if autocomplete interactions are enabled for this `STRING`, `INTEGER`, or `NUMBER` type option
- * - `autocomplete` may not be set to `true` if choices are present
- * - options using `autocomplete` are not confined to only use choices given by the application.
- */
-
-/** A command creator for the bot */
+/** A command creator for the bot. */
 class BotCmd {
-  /** A private method that sets the properties of `this` */
-  // if you see an 'Unexpected character' error: see https://stackoverflow.com/a/52237988/12180492
-  #setX (obj) {
-    for (const key of Object.keys(obj)) {
-      this[key] = obj[key]
-      this.info[key] = obj[key] // backward compatibility // todo: this is temp in the meantime
+  /**
+   * Set the properties of the command object
+   * @param {Object.<String, Any>} object
+   */
+  setX (object) {
+    // Loop on all properties of the object
+    for (const key of Object.keys(object)) {
+      this[key] = object[key] // Copy the property from the input object to `this`
+      this.info[key] = object[key] // backward compatibility // todo: this is temp in the meantime
     }
     return this
   }
 
-  // Set common command properties, usually shown in the help embed
   /**
    * Creates a new bot command with default properties
    * @param {String} name Name of the command. Must be the same as the file name.
    */
-  constructor (name) { return this.#setX({ info: {}, name, category: 'General', permLevel: 'User' }) }
-  /** @param {String} category */
-  setCategory (category) { return this.#setX({ category }) }
-  /** @param {String} thumbnail */
-  setThumb (thumbnail) { return this.#setX({ thumbnail }) }
-  /** @param {String} description */
-  setDescription (description) { return this.#setX({ description }) }
-  /** @param {String} usage */
-  setUsage (usage) { return this.#setX({ usage }) }
-  /** @param {String} option */
-  setOptionText (option) { return this.#setX({ option }) }
-  /** @param {String} similar */
-  setSimilar (similar) { return this.#setX({ similar }) }
-  /** @param {String[]} aliases */
-  setAliases (...aliases) { return this.#setX({ aliases }) }
-  /** @param {String} permLevel */
-  setPerm (permLevel) { return this.#setX({ permLevel }) }
+  constructor (name) { return this.setX({ info: {}, name, category: 'General', permLevel: 'User' }) }
+
+  // Set common command properties, usually shown in the help embed
+  /**
+   * Category of the command
+   * @param {String} category
+   */
+  setCategory (category) { return this.setX({ category }) }
+  /**
+   * Thumbnail that will be shown on the help embed
+   * @param {String} thumbnail
+   */
+  setThumbnail (thumbnail) { return this.setX({ thumbnail }) }
+  /**
+   * Description of the command
+   * @param {String} description
+   */
+  setDescription (description) { return this.setX({ description }) }
+  /**
+   * Show how the command will be used
+   * @param {String} usage
+   */
+  setUsage (usage) { return this.setX({ usage }) }
+  /**
+   * Description of the different options of the command (for Message)
+   * @param {String} option
+   */
+  setOptionText (option) { return this.setX({ option }) }
+  /**
+   * List of similar commands
+   * @param {String} similar
+   */
+  setSimilar (similar) { return this.setX({ similar }) }
+  /**
+   * Aliases or alternate names of the command
+   * @param {String[]} aliases
+   */
+  setAliases (...aliases) { return this.setX({ aliases }) }
 
   // Set command config properties
-  /** @param {Boolean} requiredArgs */
-  requiredArgs (requiredArgs = true) { return this.#setX({ requiredArgs }) }
+  /**
+   * Minimum permLevel required to use the command
+   * @param {String} permLevel
+   */
+  setPermLevel (permLevel) { return this.setX({ permLevel }) }
+  /**
+   * Are inputs or arguments required for the command to run
+   * @param {Boolean} requiredArgs
+   */
+  isRequiredArgs (requiredArgs = true) { return this.setX({ requiredArgs }) }
   /** @param {Boolean} dank */
-  isDank (dank = true) { return this.#setX({ dank }) }
+  isDank (dank = true) { return this.setX({ dank }) }
   /**
    * Set command interaction options
-   * @param {applicationCommandOption[]} options
+   * @param {import('discord.js').ApplicationCommandOption[]} options
    */
-  setOptions (...options) { return this.#setX({ options }) }
+  interactionOptions (...options) { return this.setX({ options }) }
 
-  // The main function of the command
-  /** @param {(msg: import('./sendMsg'), args: String[]) => Any} run */
-  setFunction (run) { return this.#setX({ run }) }
+  /**
+   * The main function to run
+   * @param {(msg: import('./sendMsg'), args: String[]) => Any} run
+   */
+  callback (run) { return this.setX({ run }) }
 }
 
 module.exports = BotCmd
