3bbb18cfc7ef18e1c03b897fdaa71114be4740d1
xyr11 <68799333+xyr11@users.noreply.github.com>
2022-09-05 21:08:13 +0800

    terminate the client when rebooting

--- commands/reboot.js
+++ commands/reboot.js
@@ -56,6 +56,9 @@ module.exports = new BotCmd('reboot')
       return client.commands.delete(cmd.info.name)
     }))
 
+    // Terminate connection
+    client.destroy()
+
     // Exit
     process.exit(0)
   })
